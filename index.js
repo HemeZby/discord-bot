1ed59c7aa667a29e07de03ce1c3ed524416889f0
Miguel <miguelnsxpt@gmail.com>
2021-06-19 17:09:53 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const { prefix, token } = require('./config.json');
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+
+client.on('message', message => {
+	if (message.content.startsWith(`${prefix}ping`)) {
+		message.channel.send('Pong.');
+	} else if (message.content.startsWith(`${prefix}beep`)) {
+		message.channel.send('Boop.');
+	}
+});
+
+client.login(config.token);
+
